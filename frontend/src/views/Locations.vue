b37431dd787ef66adf50fbf7a02009105f8defe2
Tjalle Wolterink <tjalle.wolterink@student.ru.nl>
2021-05-30 19:05:58 +0200

    working on input validation

--- frontend/src/views/Locations.vue
+++ frontend/src/views/Locations.vue
@@ -11,7 +11,10 @@
     </v-data-table>
 
     <v-col class="text-right">
-      <v-btn @click="deleteItem()" color="red">Delete Selection</v-btn>
+      <v-btn @click="deleteItem()" color="red" rounded>Delete Selection</v-btn>
+      <v-btn @click="addItem()" color="green" class="mx-2" rounded
+        >Add Item</v-btn
+      >
     </v-col>
 
     <v-dialog v-model="dialogDelete" max-width="500px">
@@ -19,11 +22,36 @@
         <v-card-title class="headline"
           >Are you sure you want to delete these items?</v-card-title
         >
-        <v-card-text v-for="item in selected" :key="item">{{item.name}}</v-card-text>
+        <v-card-text v-for="item in selected" :key="item">{{
+          item.name
+        }}</v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="green" text @click="closeDelete">Cancel</v-btn>
           <v-btn color="red" text @click="deleteItemConfirm">OK</v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="dialogAdd" max-width="500px">
+      <v-card>
+        <v-text-field
+          class="mx-10"
+          label="Name"
+          :rules="rulesName"
+          hide-details="auto"
+        ></v-text-field>
+        <v-text-field
+          class="mx-10"
+          label="Area"
+          :rules="rulesArea"
+          hide-details="auto"
+        ></v-text-field>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="red" text @click="closeAdd">Cancel</v-btn>
+          <v-btn color="green" text @click="addConfirm">Add</v-btn>
           <v-spacer></v-spacer>
         </v-card-actions>
       </v-card>
@@ -49,11 +77,21 @@ export default {
       ],
       locations: [],
       dialogDelete: false,
+      dialogAdd: true,
       selected: [],
       sure: false,
+      rulesName: [
+        (value) => !!value || "Required.",
+        (value) => (value && value.length >= 3) || "Min 3 characters",
+      ],
+      rulesArea: [
+        (value) => !!value || "Required.",
+        (value) => (value && Number.isInteger(value)) || "Must be an integer",
+      ],
     };
   },
   methods: {
+    addItem() {},
     deleteItem() {
       if (this.selected.length > 0) {
         this.dialogDelete = true;
